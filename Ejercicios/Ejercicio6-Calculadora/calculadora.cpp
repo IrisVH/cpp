2b26dc0ba77dd11325e3a5e4b4d47852951c77f6
IrisVH <ivhernandez@unah.hn>
2020-07-06 22:45:48 -0600

    subiendo ejercicio 6

---
+++ Ejercicios/Ejercicio6-Calculadora/calculadora.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char** argv) {
+	
+	int a=0;
+	int b=0;
+	int suma,resta,multiplicacion,division=0;
+	cout<<"Ingrese el valor de a:";cin>>a;
+	cout<<endl;
+	cout<<"Ingrese el valor de b:";cin>>b;
+	
+	suma = a+b;
+	resta = a-b;
+	multiplicacion = a*b;
+	division = a/b;
+	
+	cout<<endl;
+	
+	cout<<"La suma de a+b es:"<<suma<<endl;
+	cout<<"La resta de a-b es:"<<resta<<endl;
+	cout<<"La multiplicacion de a*b es:"<<multiplicacion<<endl;
+	cout<<"La division de a*b es:"<<division<<endl;
+	
+	
+	return 0;
+}
