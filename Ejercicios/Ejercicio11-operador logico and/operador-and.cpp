28d2f1a03506399bf46c3fb475577c9ba0052b4c
IrisVH <ivhernandez@unah.hn>
2020-07-19 10:09:44 -0600

    agregando ejercicio 11

---
+++ Ejercicios/Ejercicio11-operador logico and/operador-and.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+
+using namespace std;
+ int main(int argc, char const *argv[])
+ {
+int a=50;
+int b=10;
+int c=7;
+
+//if (a>b && a>c){
+//cout<<"a es el mayor";    
+//}else if (b >a&& b>c){
+ //   cout<<"b es el mayor"; 
+//}else 
+ //   cout<<" es el mayor";
+
+if (a>b && a>c){
+cout<<"a es el mayor";    
+}
+if (b >a&& b>c){
+   cout<<"b es el mayor"; 
+} 
+if (c >a&& c>b){
+  cout<<"c es el mayor";
+  }
+     return 0;
+ }
+ 
