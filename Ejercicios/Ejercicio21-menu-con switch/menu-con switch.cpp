2036b6869da74cf48f369a288ad86e4a7efd27e3
IrisVH <ivhernandez@unah.hn>
2020-07-22 19:52:28 -0600

    Agregando ejercicio 21

---
+++ Ejercicios/Ejercicio21-menu-con switch/menu-con switch.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+
+
+using namespace std;
+
+
+
+int main(int argc, char** argv) {
+	
+	int opcion=0;
+	
+	
+	while(true){
+		system("cls");
+		cout<<"****"<<endl;
+        cout<<"menu"<<endl;
+        cout<<"****"<<endl;
+
+        cout<<endl;
+        cout<<"1-Cafe y Granitas"<<endl;
+        cout<<"2-Reposteria"<<endl;
+        cout<<"0-salir"<<endl;
+
+        cout<<"Ingrese una opcion del menu:";
+        cin>>opcion;
+        
+        if(opcion==0){
+            break;
+        }
+        switch (opcion)
+        {
+        case 1:
+        {
+            system("cls");
+            cout<<"Estas en el menu de Cafe y Granitas"<<endl;
+            system("pause");
+            break;
+        }
+        case 2:
+           
+           {
+            system("cls");
+            cout<<"Estas en el menu de Reposteria"<<endl;
+            system("pause");
+           break;
+           }
+        
+        default:
+           {
+            cout<<"Ingrese una opcion valida(0,1,2)";
+            system("pause");
+            break;
+
+           }
+        
+        }
+
+		
+	}
+	 cout<<endl;
+     cout<<"Saliste del sistema";
+
+	
+	return 0;
+}
