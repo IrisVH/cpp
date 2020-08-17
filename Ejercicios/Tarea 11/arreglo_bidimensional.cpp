c0e7e7221cf48b9ff25bc5f6fa757496f4c7c249
IrisVH <ivhernandez@unah.hn>
2020-08-17 16:40:33 -0600

    Arreglando tarea 11

--- Ejercicios/Tarea 11/arreglo_bidimensional.cpp
+++ Ejercicios/Tarea 11/arreglo_bidimensional.cpp
@@ -14,19 +14,23 @@ int main(int argc, char const *argv[])
    srand(time(NULL));
    int suma=0;
 
-
+   cout <<"*****************************";
+   cout <<endl;
    for (int i = 0; i < 5; i++)
    { 
        for (int j = 0; j < 5; j++)
    {
-    //genera un numero entre 1 y 10
+    
    numero[i][j]= rand() % 100+1;
+
    cout << " "<<"["<<numero[i][j] << "]"<<" ";  
    suma=numero[i][j]+suma;
    }
      cout <<endl;
    }
-
+   cout <<endl;
+   cout <<"*****************************";
+   
    cout <<endl;
    cout <<endl;
    cout <<endl;
