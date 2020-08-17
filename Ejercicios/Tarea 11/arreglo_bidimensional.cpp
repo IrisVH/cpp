70020b7a5ca104ae2af24a3ecc17848ece759b4d
IrisVH <ivhernandez@unah.hn>
2020-08-17 16:34:24 -0600

    Agregando tarea 11

---
+++ Ejercicios/Tarea 11/arreglo_bidimensional.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <stdlib.h>
+#include <time.h>
+
+using namespace std;
+
+
+
+int main(int argc, char const *argv[])
+{
+
+   //inicializa el numero random 
+   int numero[5][5];  
+   srand(time(NULL));
+   int suma=0;
+
+
+   for (int i = 0; i < 5; i++)
+   { 
+       for (int j = 0; j < 5; j++)
+   {
+    //genera un numero entre 1 y 10
+   numero[i][j]= rand() % 100+1;
+   cout << " "<<"["<<numero[i][j] << "]"<<" ";  
+   suma=numero[i][j]+suma;
+   }
+     cout <<endl;
+   }
+
+   cout <<endl;
+   cout <<endl;
+   cout <<endl;
+   cout<< "La suma de los numeros de la matriz es:"<<suma;
+
+
+
+    return 0;
+}
